6ad874f63823a6303c8163380fd284aebd91d8bf
Ethikry <113566829+Ethikry@users.noreply.github.com>
2022-10-25 21:16:26 -0600

    Update to webpage with js

--- assets/script.js
+++ assets/script.js
@@ -6,3 +6,21 @@ burgerIcon.addEventListener('click', () => {
 });
 
 // #curl "https://worldtimeapi.org/api/Europe/London";
+
+let clientID="FqwsiGk0s2Nox9oBbr-hudpZcsi7sQXZRbg9PvZ-UFo";
+let endpoint="https://api.unsplash.com/photos/random/?client_id=FqwsiGk0s2Nox9oBbr-hudpZcsi7sQXZRbg9PvZ-UFo";
+//let endpoint='https://api.unsplash.com/photos/random/?client_id=${clientID}';
+
+let imageElement = document.querySelector("#unsplashImage");
+let imageLink = document.querySelector("#imageLink");
+let creator = document.querySelector("#creator");
+
+fetch(endpoint)
+    .then(function (response) {
+        console.log(response.json());
+        //return response.json();
+    })
+    .then(function (jsonData) {
+        console.log
+        //imageElement.src = jsonData.urls.full;
+    })

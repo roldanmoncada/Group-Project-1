2e75d424a62c5399e627f13c1ed5688fc4224d0c
ROSE <rosahcuenca12@gmail.com>
2022-10-25 21:33:42 -0600

    bones

--- script.js
+++ script.js
@@ -9,3 +9,16 @@ fetch("http://worldtimeapi.org/api/timezone", {
     .then(function (data) {
       console.log(data);
     });
+
+    /*function getApi() {
+      // fetch request gets a list of all the repos for the node.js organization
+      var requestUrl = 'http://worldtimeapi.org/api/timezone';
+    
+      fetch(requestUrl)
+        .then(function (response) {
+          return response.json();
+        })
+        .then(function (data) {
+          console.log(data)
+        }); 
+      }*/

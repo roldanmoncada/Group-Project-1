9c0d6267b8085b3bbcbe0d4e561120aaf34100a2
roldanmoncada <roldan.h.moncada@gmail.com>
2022-10-29 11:33:18 -0600

    began to work on time API functionality.

--- script.js
+++ script.js
@@ -54,3 +54,33 @@ async function getNewImage() {
 }
 
 
+// TIME API SECTION!!!!!!
+
+fetch("http://worldtimeapi.org/api/timezone", {
+    method: 'GET', 
+    credentials: 'same-origin', 
+    redirect: 'follow', 
+})
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+    });
+
+    /*function getApi() {
+      // fetch request gets a list of all the repos for the node.js organization
+      var requestUrl = 'http://worldtimeapi.org/api/timezone';
+    
+      fetch(requestUrl)
+        .then(function (response) {
+          return response.json();
+        })
+        .then(function (data) {
+          console.log(data)
+        }); 
+      }*/
+
+var timeRequestUrl = "http://worldtimeapi.org/api";
+console.log(timeRequestUrl)
+
